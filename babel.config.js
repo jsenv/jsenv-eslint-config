1412830984f6a39a403afdf44651e783ab290826
Damien Maillard <damien.maillard@dailymotion.com>
2019-03-23 09:48:59 +0100

    use babel eslint beta to support top level await

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,8 @@ const syntaxDynamicImport = require("@babel/plugin-syntax-dynamic-import")
 const syntaxImportMeta = require("@babel/plugin-syntax-import-meta")
 
 module.exports = {
+  parserOpts: {
+    allowAwaitOutsideFunction: true,
+  },
   plugins: [syntaxDynamicImport, syntaxImportMeta],
 }
