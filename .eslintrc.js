c1cc73bd0de462195e2cb7e928c069b1d4e21c53
Damien Maillard <damien.maillard@dailymotion.com>
2019-12-09 12:52:02 +0100

    update self dep

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
-const { createConfig } = require("@jsenv/eslint-config")
+const { createEslintConfig } = require("@jsenv/eslint-config")
+const jsenvConfig = require("./jsenv.config.js")
 
-const config = createConfig({
-  projectDirectoryPath: __dirname,
+const config = createEslintConfig({
+  ...jsenvConfig,
   importResolutionMethod: "import-map",
-  jsxEnabled: true,
-  reactPluginEnabled: true,
+  jsx: true,
+  react: true,
 })
 
 module.exports = config
