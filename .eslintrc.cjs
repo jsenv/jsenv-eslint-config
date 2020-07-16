18f7d73c83da1233ec693ffe8d0550092ddd7377
Damien Maillard <damien.maillard@dailymotion.com>
2020-07-16 13:14:15 +0200

    update dep to importmap-eslint-resolver

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,7 @@ const { createEslintConfig } = require("@jsenv/eslint-config")
 const config = createEslintConfig({
   projectDirectoryUrl: __dirname,
   importResolutionMethod: "import-map",
+  importMapFileRelativeUrl: "./import-map.importmap",
   jsx: true,
   react: true,
 })
